b29c41acda9b996f6c93b7d768da51361b8d6207
Jordan <jordan.m.edginton@gmail.com>
2022-02-18 19:47:30 -0700

    added questions, variables and start/getquestions functions

---
+++ assets/js/questions.js
@@ -0,0 +1,19 @@
+// List of Questions for the quiz
+var questions = [
+    {
+    title: "What is JavaScript?",
+    choices: ["JavaScript is the Programming Language for the Web", "A script with java (coffee) spilled on it", "JavaScript is not a real word"],
+    answer: "JavaScript is the Programming Language for the Web"
+  },
+
+    {
+        title: "What is a string in JavaScript?",
+        choices: ["It is used for sewing to create clothing", "A sequence of one or more characters that may consist of letters, numbers, or symbols", "A material cats like to play with"],
+        answer: "A sequence of one or more characters that may consist of letters, numbers, or symbols"
+  },
+    {
+        title: "What does JSON.stringify do?",
+        choices: ["Creates a GIF of a guy named Jason playing with string similar to a cat would", "Son of J", "Converts a JavaScript object or value to a JSON string"],
+        answer: "Converts a JavaScript object or value to a JSON string"
+  },
+];
